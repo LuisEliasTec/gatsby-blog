a285f83f766bb506468c7d95ab5c685a23d227c5
LuisEliasTec <luiselias_1@hotmail.com>
2021-12-24 18:30:01 -0700

    Second commit

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function about() {
+    return (
+        <Layout>
+            <div>
+                <h1>About page</h1>
+                <p>Fugiat pariatur exercitation occaecat est non aute dolore laborum nostrud non cillum aliquip exercitation. Cupidatat ullamco eu consectetur aute velit ad adipisicing occaecat Lorem nulla exercitation elit consequat cupidatat. Nostrud officia consequat laboris fugiat dolore ipsum nostrud excepteur anim tempor laborum dolore mollit. Exercitation deserunt ea incididunt ea anim aute. Commodo eu in dolore voluptate excepteur consectetur ullamco proident fugiat cillum incididunt et voluptate. In qui voluptate nisi tempor proident.</p>
+                <p>Fugiat pariatur exercitation occaecat est non aute dolore laborum nostrud non cillum aliquip exercitation. Cupidatat ullamco eu consectetur aute velit ad adipisicing occaecat Lorem nulla exercitation elit consequat cupidatat. Nostrud officia consequat laboris fugiat dolore ipsum nostrud excepteur anim tempor laborum dolore mollit. Exercitation deserunt ea incididunt ea anim aute. Commodo eu in dolore voluptate excepteur consectetur ullamco proident fugiat cillum incididunt et voluptate. In qui voluptate nisi tempor proident.</p>
+                <p>Fugiat pariatur exercitation occaecat est non aute dolore laborum nostrud non cillum aliquip exercitation. Cupidatat ullamco eu consectetur aute velit ad adipisicing occaecat Lorem nulla exercitation elit consequat cupidatat. Nostrud officia consequat laboris fugiat dolore ipsum nostrud excepteur anim tempor laborum dolore mollit. Exercitation deserunt ea incididunt ea anim aute. Commodo eu in dolore voluptate excepteur consectetur ullamco proident fugiat cillum incididunt et voluptate. In qui voluptate nisi tempor proident.</p>
+            </div>
+        </Layout>
+    )
+}
