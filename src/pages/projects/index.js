ae7adf78536ab9d101b77c5ab778b608f2ce17e5
LuisEliasTec <luiselias_1@hotmail.com>
2021-12-30 22:23:18 -0700

    Use a multiples queries with Graphql

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,48 @@
-import React from 'react'
-import Layout from '../../components/Layout'
-import * as styles from '../../styles/projects.module.css'
+import React from "react"
+import Layout from "../../components/Layout"
+import * as styles from "../../styles/projects.module.css"
+import { graphql, Link } from "gatsby"
 
-export default function Projects() {
-    return (
-        <Layout>
-            <div className={styles.portfolio}>
-                <h2>Portfolio</h2>
-                <h3>Projects & Websites I've Created</h3>
-            </div>
-        </Layout>
-    )
+export default function Projects({ data }) {
+  console.log(data)
+  const projects = data.projects.nodes
+  const contact = data.contact.siteMetadata.contact
+  return (
+    <Layout>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
+        <div class={styles.projects}>
+          {projects.map(project => (
+            <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
+              <h3>{project.frontmatter.title}</h3>
+              <p>{project.frontmatter.stack}</p>
+            </Link>
+          ))}
+        </div>
+        <p>Like what you see? Email me at {contact} for a quote.</p>
+      </div>
+    </Layout>
+  )
 }
+
+// export page query
+export const query = graphql`
+  query ProjectsPage {
+    projects: allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          stack
+          slug
+        }
+        id
+      }
+    }
+    contact: site {
+      siteMetadata {
+        contact
+      }
+    }
+  }
+`
---
+++ src/projects/dojo-coffe-house.md
@@ -0,0 +1,16 @@
+---
+title: The Dojo Coffe House
+stack: HTML & CSS
+slug: the-dojo-coffe-house
+date: 2021-01-01T00:00:00+00:00
+---
+
+**Commodo sint Lorem anim amet incididunt cupidatat irure laboris.**, Velit mollit amet esse dolor cillum quis duis duis. Officia tempor anim exercitation mollit. Et pariatur Lorem duis tempor qui ex. Veniam excepteur ex minim commodo fugiat pariatur Lorem non qui eiusmod ullamco voluptate ullamco. Voluptate do dolore sit proident labore nisi proident in ut cillum in.
+
+Mollit nostrud sit nisi mollit enim ipsum cillum in deserunt eu amet aute. Nisi Lorem veniam id eiusmod ullamco. Commodo eiusmod nulla deserunt exercitation sint aute reprehenderit consectetur eu ad. Esse qui dolore exercitation do amet aliqua. Lorem minim laborum aute aute quis laboris. Est et officia ullamco aute do officia nostrud excepteur est sint.
+
+Enim ex elit voluptate velit cupidatat dolore duis commodo in. Duis aliqua officia elit magna dolore. Adipisicing Lorem minim est ex. Est minim et ad laborum est commodo reprehenderit nisi. Dolore anim in nisi magna non nulla excepteur eu cupidatat consectetur adipisicing.
+
+Laboris proident consectetur sint deserunt commodo in amet esse in ullamco culpa. Aliqua enim do deserunt in. Ex deserunt cillum cupidatat nostrud commodo. Occaecat duis consequat cillum anim nisi amet consectetur laborum dolor elit amet ex labore minim. Esse incididunt excepteur cupidatat mollit nulla eiusmod officia culpa nostrud ea eiusmod sint dolore tempor.
+
+### Fugiat veniam ut cupidatat sit ipsum est elit.
